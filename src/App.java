036da13cb6b1a79a60cf61816f0f698c7eff5960
MahmooudGaaber <mahmoud.gr231@gmail.com>
2025-11-17 14:47:33 +0200

    Linear Search

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+    // * code of Linear Search
+    static void doLinearSearch(int[] dataArray, int target){
+        // * Loop to go throw all items on List
+       for (int i = 0 ; i < dataArray.length ; i ++ ){
+           // * checked the data if found and print it and stop app then
+            if (dataArray[i] == target) {
+                System.out.println("Found It" + i);
+                return;
+            }
+            // * checked the data if Not found
+            else {
+                System.out.println("Not Found It");
+                return;
+            }
+        }
     }
+    
+    public static void main(String[] args) throws Exception {
+
+        // ? Call Linear Search Method
+        int[] linearData = new int[]{1, 2, 3, 4, 5};
+        doLinearSearch(linearData,2);
+
+
+    }
+
+
+            
+
 }
